b9cb0ed8048203c1a98fd888f9a5d23aa0df634a
Enzo Bozzani <bozzanienzo@gmail.com>
2024-09-26 18:29:00 -0300

    feat: script to drop all collections then transfer data from postgres to mongo

---
+++ app.py
@@ -0,0 +1,89 @@
+# Standard
+from typing import Any
+import os
+from decimal import Decimal
+
+# Third Party
+import psycopg2
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+postgres_conn = psycopg2.connect(os.environ['POSTGRES_URL'])
+mongo_conn = MongoClient(os.environ['MONGO_URL'])
+mongo_db = mongo_conn.projeto_db
+
+def show_tables() -> list[str]:
+    with postgres_conn.cursor() as db:
+        db.execute("SHOW TABLES;")
+        res = db.fetchall()
+        postgres_conn.commit()
+        return [table[1] for table in res]
+
+def select_all(table_name: str) -> list[Any]:
+    with postgres_conn.cursor() as db:
+        db.execute(f"SELECT * FROM {table_name};")
+        res = db.fetchall()
+        postgres_conn.commit()
+        return res
+    
+def select_columns(table_name: str) -> list[Any]:
+    with postgres_conn.cursor() as db:
+        db.execute(f"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='{table_name}';")
+        res = db.fetchall()
+        postgres_conn.commit()
+        return [table[3] for table in res]
+    
+def transfer_data():
+    print("Fetching table names...")
+    tables = show_tables()
+
+    print("\n-----------------------------------------------------------------------------\n")
+
+    for table in tables:
+        print(f"Selecting table {table} columns names...")
+        cols = select_columns(table)
+        print(f"Selecting table {table} records ...")
+        rows = select_all(table)
+
+        records = []
+
+        for row in rows:
+            formatted = []
+            for value in row:
+                if isinstance(value, Decimal):
+                    formatted.append(float(value))
+                else:
+                    formatted.append(value)   
+
+            records.append(dict(zip(cols, formatted)))
+
+        try:
+            print(f"Creating {table} collection in MongoDB")
+            mongo_db.create_collection(table)
+        except:
+            pass
+        
+        print(f"Inserting values in {table} collection")
+        mongo_db[table].insert_many(records)
+
+        print("\n-----------------------------------------------------------------------------\n")
+    
+    print("Data transference completed!")
+
+def drop_all_collections():
+    collections = mongo_db.list_collection_names()
+
+    if len(collections) == 0:
+        return
+
+    print("Deleting all collections...")
+    for collection in mongo_db.list_collection_names():
+        mongo_db[collection].drop()
+        print(f"Deleted {collection} collection successfully")
+        
+
+if __name__ == '__main__':
+    drop_all_collections()
+    transfer_data()
